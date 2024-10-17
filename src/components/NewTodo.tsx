10f7424ca364585574bb47afb753517cab8cfdd3
alina-sadykova <asadykovaa@gmail.com>
2024-10-16 22:58:53 -0500

    complete todo app

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,31 @@
+import { useContext, useRef } from "react";
+
+import { TodosContext } from "../store/todos-context";
+import classes from "./NewTodo.module.css";
+
+const NewToDo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText?.trim().length === 0) {
+      return;
+    }
+
+    todosCtx.addTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add todo</button>
+    </form>
+  );
+};
+export default NewToDo;
